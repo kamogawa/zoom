5d8400565622a98ec35526bdb80c18a77152582e
BEUMGOO <captain_kbg@naver.com>
2021-09-26 23:50:34 +0900

    カメラチェンジ

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,15 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (e) {
@@ -25,14 +30,23 @@ async function getCameras() {
 }
 
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -64,4 +78,8 @@ cameraBtn.addEventListener("click", () => {
     cameraBtn.innerText = "カメラ On";
     cameraOff = true;
   }
+});
+
+camerasSelect.addEventListener("input", () => {
+  await getMedia(camerasSelect.value);
 });
